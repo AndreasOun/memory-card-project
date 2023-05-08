2ac96d6e794016f2f85def358db4c42a65d9b615
AndreasOun <andreastthk@gmail.com>
2023-05-08 03:46:17 +0300

    Updates

---
+++ precache-manifest.8a0fb42ff8cec85bec7d2a162d103542.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "443eed35e1e3f317261a",
+    "url": "/memory-card-project/static/css/main.3314d3d7.chunk.css"
+  },
+  {
+    "revision": "443eed35e1e3f317261a",
+    "url": "/memory-card-project/static/js/main.443eed35.chunk.js"
+  },
+  {
+    "revision": "51323e8beb036e2e2c74",
+    "url": "/memory-card-project/static/js/1.51323e8b.chunk.js"
+  },
+  {
+    "revision": "18a63ae2f21b75a6b1dd",
+    "url": "/memory-card-project/static/js/runtime~main.18a63ae2.js"
+  },
+  {
+    "revision": "5e11e98e0c95240eb3ee5812ffba07e4",
+    "url": "/memory-card-project/static/media/background.5e11e98e.jpg"
+  },
+  {
+    "revision": "854bc7ff5798791b9f24a6eaf41d7c59",
+    "url": "/memory-card-project/static/media/A.854bc7ff.PNG"
+  },
+  {
+    "revision": "9c955310b6c234b8e662b60c0569e2dd",
+    "url": "/memory-card-project/static/media/B.9c955310.PNG"
+  },
+  {
+    "revision": "c2e7cb3fdbf671292c44093cb5478edf",
+    "url": "/memory-card-project/static/media/C.c2e7cb3f.PNG"
+  },
+  {
+    "revision": "42d5893a42ab60b1576bf6868a6be2f6",
+    "url": "/memory-card-project/static/media/D.42d5893a.PNG"
+  },
+  {
+    "revision": "173040ba714442b1d958f27aa43306b2",
+    "url": "/memory-card-project/static/media/E.173040ba.PNG"
+  },
+  {
+    "revision": "187f25314c905dd07ef098707942f5b1",
+    "url": "/memory-card-project/static/media/F.187f2531.PNG"
+  },
+  {
+    "revision": "9b3a1c87185db2622a31faba2924f81d",
+    "url": "/memory-card-project/static/media/G.9b3a1c87.PNG"
+  },
+  {
+    "revision": "dc363010e2a7aa5dc2a8ecae0634f844",
+    "url": "/memory-card-project/static/media/H.dc363010.PNG"
+  },
+  {
+    "revision": "aba38834a3fcc71b8d951cfe635b7217",
+    "url": "/memory-card-project/static/media/bg-music.aba38834.mp3"
+  },
+  {
+    "revision": "7c22e0acc3f597cedac253063883f811",
+    "url": "/memory-card-project/index.html"
+  }
+];
